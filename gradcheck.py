0dca6870f8e89bc2861c91d795db54d9738a9696
Unknown <ayalondean@gmail.com>
2017-03-23 11:35:09 +0200

    Basics

--- gradcheck.py
+++ gradcheck.py
@@ -2,6 +2,14 @@
 
 import numpy as np
 import random
+from sigmoid import sigmoid, sigmoid_grad
+
+
+def sigmoid_function(x):
+    val = sigmoid(x)
+    grad = sigmoid_grad(val)
+
+    return val, grad
 
 
 # First implement a gradient checker by filling in the following functions
@@ -23,15 +31,14 @@ def gradcheck_naive(f, x):
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
-
+        print("ix is " + str(ix))
         # Try modifying x[ix] with h defined above to compute
         # numerical gradients. Make sure you call random.setstate(rndstate)
         # before calling f(x) each time. This will make it possible
         # to test cost functions with built in randomness later.
 
-        ### YOUR CODE HERE:
-        raise NotImplementedError
-        ### END YOUR CODE
+        #numgrad = ((fx[ix] + h) - (fx[ix] - h)) / 2*h
+        numgrad = (f(x[ix] + h)[0] - (f(x[ix] - h)[0])) / (2*h)
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
